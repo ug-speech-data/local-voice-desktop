494bb958dd5ff4246f0680df74b7a65896e7d92a
dodziraynard <dodzireynard@gmail.com>
2024-09-01 10:43:19 +0100

    update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,26 +10,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          // App Bar
           AppNavBar(),
           const MainPage(
             title: '',
           )
-          // Main page
         ],
       ),
     );
